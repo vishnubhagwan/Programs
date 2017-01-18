88b9c49d8c26bb176919c06f02150c3a09431ed2
govind <you@example.com>
2017-01-18 18:43:45 +0530

    dsu and number theory updated

--- lib.cpp
+++ lib.cpp
@@ -22,6 +22,7 @@ using namespace std;
 #define out(a) cout << a << '\n'
 #define set(a) memset(a,0,sizeof(a))
 typedef long long int ll;
+typedef pair<int, int> ii;
 
 ll p(ll a, ll b)
 {
@@ -36,13 +37,13 @@ ll ans;
 struct DSU
 {
 	int S;
-	
+
 	struct node
 	{
 		int p; ll sum;
 	};
 	vector<node> dsu;
-	
+
 	DSU(int n)
 	{
 		S = n;
@@ -54,7 +55,7 @@ struct DSU
 		}
 		ans=n;
 	}
-	
+
 	void reset(int n)
 	{
 		dsu.clear();
@@ -66,14 +67,14 @@ struct DSU
 			dsu.pb(tmp);
 		}
 	}
-	
+
 	int rt(int u)
 	{
 		if(dsu[u].p == u) return u;
 		dsu[u].p = rt(dsu[u].p);
 		return dsu[u].p;
 	}
-	
+
 	void merge(int u, int v)
 	{
 		u = rt(u); v = rt(v);
@@ -82,6 +83,175 @@ struct DSU
 		dsu[v].p = u;
 		ans+=dsu[u].sum*dsu[v].sum*2LL;
 		dsu[u].sum += dsu[v].sum;
+	}
+
+	bool sameset(int u, int v)
+	{
+		if(rt(u) == rt(v)) return true;
+		return false;
+	}
+
+	ll getstat(int u)
+	{
+		return dsu[rt(u)].sum;
+	}
+};
+
+struct NumberTheory
+{
+	vector<ll> primes;
+	vector<bool> prime;
+	vector<ll> totient;
+	vector<ll> sumdiv;
+	vector<ll> bigdiv;
+	void Sieve(ll n)
+	{
+		prime.assign(n+1, 1);
+		prime[1] = false;
+		for(ll i = 2; i <= n; i++)
+		{
+			if(prime[i])
+			{
+				primes.pb(i);
+				for(ll j = i*2; j <= n; j += i)
+				{
+					prime[j] = false;
+				}
+			}
+		}
+	}
+
+	ll phi(ll x)
+	{
+		map<ll,ll> pf;
+		ll num = 1; ll num2 = x;
+		for(ll i = 0; primes[i]*primes[i] <= x; i++)
+		{
+			if(x%primes[i]==0)
+			{
+				num2/=primes[i];
+				num*=(primes[i]-1);
+			}
+			while(x%primes[i]==0)
+			{
+				x/=primes[i];
+				pf[primes[i]]++;
+			}
+		}
+		if(x>1)
+		{
+			pf[x]++; num2/=x; num*=(x-1);
+		}
+		x = 1;
+		num*=num2;
+		return num;
+	}
+
+	bool isprime(ll x)
+	{
+		if(x==1) return false;
+		for(ll i = 0; primes[i]*primes[i] <= x; i++)
+		{
+			if(x%primes[i]==0) return false;
+		}
+		return true;
+	}
+
+	void SievePhi(ll n)
+	{
+		totient.resize(n+1);
+		for (int i = 1; i <= n; ++i) totient[i] = i;
+		for (int i = 2; i <= n; ++i)
+		{
+			if (totient[i] == i)
+			{
+				for (int j = i; j <= n; j += i)
+				{
+					totient[j] -= totient[j] / i;
+				}
+			}
+		}
+	}
+
+	void SieveSumDiv(ll n)
+	{
+		sumdiv.resize(n+1);
+		for(int i = 1; i <= n; ++i)
+		{
+			for(int j = i; j <= n; j += i)
+			{
+				sumdiv[j] += i;
+			}
+		}
+	}
+
+	ll getPhi(ll n)
+	{
+		return totient[n];
+	}
+
+	ll getSumDiv(ll n)
+	{
+		return sumdiv[n];
+	}
+
+	ll modpow(ll a, ll b, ll mod)
+	{
+		ll r = 1;
+		if(b < 0) b += mod*100000LL;
+		while(b)
+		{
+			if(b&1) r = (r*a)%mod;
+			a = (a*a)%mod;
+			b>>=1;
+		}
+		return r;
+	}
+
+	ll inv(ll a, ll mod)
+	{
+		return modpow(a, mod - 2, mod);
+	}
+
+	ll invgeneral(ll a, ll mod)
+	{
+		ll ph = phi(mod);
+		ph--;
+		return modpow(a, ph, mod);
+	}
+
+	void getpf(vector<ii>& pf, ll n)
+	{
+		for(ll i = 0; primes[i]*primes[i] <= n; i++)
+		{
+			int cnt = 0;
+			while(n%primes[i]==0)
+			{
+				n/=primes[i]; cnt++;
+			}
+			pf.pb(ii(primes[i], cnt));
+		}
+		if(n>1)
+		{
+			pf.pb(ii(n, 1));
+		}
+	}
+
+	//ll op;
+	void getDiv(vector<ll>& div, vector<ii>& pf, ll n, int i)
+	{
+		//op++;
+		ll x, k;
+		if(i >= pf.size()) return ;
+		x = n;
+		for(k = 0; k <= pf[i].s; k++)
+		{
+			if(i==int(pf.size())-1) div.pb(x);
+			getDiv(div, pf, x, i + 1);
+			x *= pf[i].f;
+		}
+	}
+};
 
 int main()
 {
